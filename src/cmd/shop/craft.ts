5719020233974a15a535744f1f29ef1c737ca382
Junhao Zhang <junhaodev@gmail.com>
2021-03-05 11:35:23 -0600

    Add crafting command

---
+++ src/cmd/shop/craft.ts
@@ -0,0 +1,17 @@
+import * as Discord from "discord.js";
+import { BigNumber as Big } from "bignumber.js";
+import { Command, Colors, Bot, Database } from "../../global";
+
+class C extends Command {
+  names = ["craft", "create"];
+  help = "Create an item... from other items!";
+  examples = ["create ego-block"];
+
+  isAdmin = true;
+
+  exec(msg: Discord.Message, args: string[]) {
+    msg.channel.send("soon! Working on adding craft data.");
+  }
+}
+
+export const c = new C();
