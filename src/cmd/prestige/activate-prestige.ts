9cc7eddde79f67c88d58e169137d1080016dd65d
Junhao Zhang <junhaodev@gmail.com>
2021-03-10 16:07:32 -0600

    Change prestige template

--- src/cmd/prestige/activate-prestige.ts
+++ src/cmd/prestige/activate-prestige.ts
@@ -1,9 +1,16 @@
 import * as Discord from "discord.js";
-import { Command, Colors, Bot, brackets } from "../../global";
-
+import { BigNumber as Big } from "bignumber.js";
+import { Command, Colors, Bot, Database } from "../../global";
 
 class C extends Command {
-  names = ["use-prestige"];
-  help = "Prestige, (In progress)."
-  isGame = 'n' as 'n';
+  names = ["prestige", "use-prestige", "prestige-reset"];
+  help = "Reset all your data and get a boost!";
+
+  isAdmin = true;
+
+  exec(msg: Discord.Message, args: string[], _: Discord.Client) {
+    msg.channel.send("soon");
+  }
 }
+
+export const c = new C();
