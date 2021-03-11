569f8fb962ab8306f51181253c3e5cfb89bd6003
Junhao Zhang <junhaodev@gmail.com>
2021-03-11 09:45:49 -0600

    Added crafting table

--- src/cmd/shop/craft.ts
+++ src/cmd/shop/craft.ts
@@ -1,19 +1,46 @@
 import * as Discord from "discord.js";
 import { BigNumber as Big } from "bignumber.js";
-import { Command, Colors, Bot, Database } from "../../global";
-import { CraftItem } from "../../util/data/craft";
+import { Command, Colors, Bot, Database, brackets } from "../../global";
+import { items } from "../../util/data/item";
+import { craftItems } from "../../util/data/craft";
 
 class C extends Command {
   names = ["craft", "create"];
   help = "Create an item... from other items!";
-  examples = ["create ego-block"];
+  examples = ["create 'chest chest chest' 5"];
 
   isAdmin = true;
 
   exec(msg: Discord.Message, args: string[]) {
-    if (args.length > 1) return Bot.argserror(msg, args.length, [0, 1]);
+    if (args.length > 3) return Bot.argserror(msg, args.length, [0, 1, 2]);
     if (args.length == 0) {
-      
+      let fields = craftItems.map(p => {
+        let uses = p.requires.map(n => `${items[n.type].name} x${brackets(n.amt.toString())}`);
+        return {
+          make: items[p.creates].name + "\n".repeat(uses.length - 1),
+          uses: uses.join("\n"),
+        }
+      });
+      Bot.carousel(msg, fields, 3, (page, i) => {
+        return {
+          color: Colors.PRIMARY,
+          title: "Crafting Items",
+          description: `These are all the available crafting items! Page ${brackets(page.toString())}`,
+          fields: i.length == 0 ? [{
+            name: "Empty Page",
+            value: "No items here!"
+          }] : [{
+            name: "Item",
+            value: i.map(n => n.make).join("\n\n"),
+            inline: true
+          }, {
+            name: "Ingredients",
+            value: i.map(n => n.uses).join("\n\n"),
+            inline: true
+          }],
+          footer: { text: "Tip: type in the item name to craft it!" }
+        }
+      });
     } else {
 
     }
--- src/util/data/craft.ts
+++ src/util/data/craft.ts
@@ -9,7 +9,7 @@ export interface CraftItem {
   requires: { amt: number | string, type: number }[];
 };
 
-export let items: CraftItem[] = [{
+export let craftItems: CraftItem[] = [{
   message: "You smash all the ego-coins together with the cheap iPhone!",
   creates: ItemEnum.ApplePhone,
   requires: [{ amt: 2, type: ItemEnum.CheapPhone }, { amt: 5, type: ItemEnum.EgoCoin }]
