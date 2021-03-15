e25b11d117b094a6afc6595897f6f44a06460b54
FunnyLamma <65251658+FunnyLamma@users.noreply.github.com>
2021-03-15 10:08:26 -0700

    Create lore.ts

---
+++ src/util/data/lore.ts
@@ -0,0 +1,17 @@
+export interface loreItem {
+
+name: string;
+description: string;
+
+}
+
+export enum LoreEnum{
+EgocoinLore
+}
+
+export const loreItemlist: loreItem[] [{
+
+name: "Ego Coin Lore"
+description: "This is the lore of Ego Coins,"
+
+}];
