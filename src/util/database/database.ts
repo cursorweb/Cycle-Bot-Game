cee85f24ca086a904a2d54db6dbcf2735b47d45f
Junhao Zhang <junhaodev@gmail.com>
2021-03-22 11:15:40 -0500

    Fixed bug where you couldn't log in

--- src/util/database/database.ts
+++ src/util/database/database.ts
@@ -1,4 +1,5 @@
 import * as fs from "fs";
+import * as path from "path";
 import admin from "firebase-admin";
 
 if (process.env.FIREBASE) {
@@ -6,7 +7,7 @@ if (process.env.FIREBASE) {
     credential: admin.credential.cert(JSON.parse(process.env.FIREBASE))
   });
 } else {
-  const serviceAccount: admin.ServiceAccount = JSON.parse(fs.readFileSync("../../../sdk-key.json", "utf8")); // i couldn't think of a better way lol
+  const serviceAccount: admin.ServiceAccount = JSON.parse(fs.readFileSync(path.join(__dirname, "../../../sdk-key.json"), "utf8")); // i couldn't think of a better way lol
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
