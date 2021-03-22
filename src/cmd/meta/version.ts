f556395cda01461acec6d0f0ca3ce052168db943
Junhao Zhang <junhaodev@gmail.com>
2021-03-22 14:48:39 -0500

    Update version notice

--- src/cmd/meta/version.ts
+++ src/cmd/meta/version.ts
@@ -10,8 +10,8 @@ class C extends Command {
     msg.channel.send({
       embed: {
         color: Colors.PRIMARY,
-        title: "Version 0.1.7",
-        description: "You can now view other people's inventory!",
+        title: "Version 0.1.8",
+        description: "The shop now has many new items, and prices are on sale!! Also, new inventory items have been added.",
         fields: [{
           name: "Discord Server",
           value: "Talk to the developers, and get sneak peeks in the official [discord server](https://discord.gg/4vTPWdpjFz)!"
