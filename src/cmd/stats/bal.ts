052a366a2ff81e8110366680a45a7b34ffef0f95
Junhao Zhang <junhaodev@gmail.com>
2021-03-28 20:40:18 -0500

    Updated bal message

--- src/cmd/stats/bal.ts
+++ src/cmd/stats/bal.ts
@@ -15,7 +15,10 @@ class C extends Command {
         title: "Personal Balance",
         description: `**Cycles**: ${brackets(commanum(user.cycles))}
 **Text**: ${brackets(commanum(user.text))}
-**Level**: ${brackets(commanum(user.level))} (${commanum(user.xp)} / ${commanum(new Big(user.level).pow(2).toString())} xp)`
+**Level**: ${brackets(commanum(user.level))} (${commanum(user.xp)} / ${commanum(new Big(user.level).pow(2).toString())} XP)`,
+        footer: {
+          text: "Tip: Use &prof to view other peoples' profiles!"
+        }
       }
     });
   }
