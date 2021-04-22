489031552eacf4a7622b72c972321b0793da6c28
Junhao Zhang <junhaodev@gmail.com>
2021-04-22 18:49:47 -0500

    Update version

--- src/cmd/meta/version.ts
+++ src/cmd/meta/version.ts
@@ -10,8 +10,8 @@ class C extends Command {
     msg.channel.send({
       embed: {
         color: Colors.PRIMARY,
-        title: "Version 0.1.11",
-        description: "Fixed a lot of exploits from version 0.1.10!! Thanks Cyanide#8020!",
+        title: "Version 0.1.12",
+        description: "Added spells! Soon for a boost shop!",
         fields: [{
           name: "Discord Server",
           value: "Talk to the developers, and get sneak peeks in the official [discord server](https://discord.gg/4vTPWdpjFz)!"
