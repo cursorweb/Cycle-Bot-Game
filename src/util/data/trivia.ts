a00ebd1ded65fefcebf38bbb8f577ad486d6d34c
Junhao Zhang <junhaodev@gmail.com>
2021-06-15 11:39:36 -0500

    Added some more trivia

--- src/util/data/trivia.ts
+++ src/util/data/trivia.ts
@@ -5,21 +5,27 @@ export interface TriviaItem {
 }
 
 export const trivia: TriviaItem[] = [{
-  code: `console.log(a);
+  code: `console
+  .log(a);
 let a = 5;`,
-  line: 1,
+  line: 2,
   lang: "JavaScript"
 }, {
   code: `#include <iostream>
 int main() {
-  cout << "Hello, world!" << endl;
+  cout <<
+  "Hello, world!" << endl;
+  return 0;
 }`,
   line: 3,
   lang: "C++"
 }, {
   code: `using System;
 class MainClass {
-  static void Main(string[] args) {
+  static
+  void
+  Main
+  (string[] args) {
     Console.WriteLine("Hello, world!");
   }
 }`,
@@ -27,8 +33,13 @@ class MainClass {
   lang: "C#"
 }, {
   code: `a = True
-print(if a == True 'a is true' else 'a is false')`,
-  line: 2,
+print(
+  if a == True
+    'a is true'
+  else
+    'a is false'
+)`,
+  line: 3,
   lang: "Python"
 }, {
   code: `var leaderboard = {
@@ -37,7 +48,70 @@ print(if a == True 'a is true' else 'a is false')`,
   nou: { score: 8 }
 };
 
-console.log(leaderboard.coder100.score);`,
-  line: 7,
+console.log(leaderboard
+  .coder100
+  .score
+);`,
+  line: 9,
+  lang: "JavaScript"
+}, {
+  code: `a = 5
+b = "3"
+
+print(
+  a
+  +
+  b
+)`,
+  line: 5,
+  lang: "Python"
+}, {
+  code: `a = int(input("Enter a number: ")
+b = int(input("Enter another one: "))
+
+print(f"{a} + {b} = {a + b}")`,
+  line: 1,
+  lang: "Python"
+}, {
+  code: `a = 7
+b = 6
+
+if b == a:
+  print("something really bad just happened!")
+ print("is your computer broken?")`,
+  line: 6,
+  lang: "Python"
+}, {
+  code: `#include <iostream>
+int main() {
+  std::string a = "hi";
+  switch (a) {
+    case "hello":
+      std::cout << "a is hello" << std::endl;
+      break;
+    default:
+      std::cout << "idk" << std::endl;
+      break;
+  }
+}`,
+  line: 4,
+  lang: "C++"
+}, {
+  code: `a = "3"
+b = "4"
+
+print(
+  "Hello"
+  " World",
+  a
+  b
+)`,
+  line: 8,
+  lang: "Python"
+}, {
+  code: `let a;
+a.b = 5;
+console.log(a.b);`,
+  line: 2,
   lang: "JavaScript"
 }];
