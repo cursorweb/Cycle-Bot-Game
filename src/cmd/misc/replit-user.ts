d56b812830e616a2c99ff197e60cdc249bc6665e
Junhao Zhang <junhaodev@gmail.com>
2021-06-19 12:08:47 -0500

    Changed command error

--- src/cmd/misc/replit-user.ts
+++ src/cmd/misc/replit-user.ts
@@ -13,54 +13,53 @@ class C extends Command {
   }
 
   async exec(msg: Discord.Message, args: string[], _: Discord.Client) {
-    if (args.length != 1) Bot.argserror(msg, args.length, [1]);
+    if (args.length != 1) return Bot.argserror(msg, args.length, [1]);
+
+    const data = await fetch("https://repl.it/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Origin": "https://repl.it",
+        "X-Requested-With": "XMLHttpRequest",
+        "User-Agent": "Mozilla/2.0"
+      },
+      body: JSON.stringify({
+        query: "query user($name:String!){userByUsername(username:$name){username,url,id,karma,image,bio,languages(limit:3){displayName,tagline}}}",
+        variables: {
+          name: args[0]
+        }
+      })
+    }).then(e => e.json());
+
+    const user: {
+      username: string, url: string, id: number,
+      karma: number, image: string, bio: string,
+      languages: { displayName: string, tagline: string }[]
+    } = data.data.userByUsername;
+
+    if (user == null) Bot.usererr(msg, `The user ${brackets(args[0])} could not be found.\nCheck your spelling!`, "User not found!");
     else {
-      const data = await fetch("https://repl.it/graphql", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Origin": "https://repl.it",
-          "X-Requested-With": "XMLHttpRequest",
-          "User-Agent": "Mozilla/2.0"
-        },
-        body: JSON.stringify({
-          query: "query user($name:String!){userByUsername(username:$name){username,url,id,karma,image,bio,languages(limit:3){displayName,tagline}}}",
-          variables: {
-            name: args[0]
-          }
-        })
-      }).then(e => e.json());
-
-      const user: {
-        username: string, url: string, id: number,
-        karma: number, image: string, bio: string,
-        languages: { displayName: string, tagline: string }[]
-      } = data.data.userByUsername;
-
-      if (user == null) Bot.usererr(msg, `The user ${brackets(args[0])} could not be found.\nCheck your spelling!`, "User not found!");
-      else {
-        msg.channel.send({
-          embed: {
-            color: Colors.SUCCESS,
-            title: `User card for ${brackets(user.username)}`,
-            author: {
-              url: `https://repl.it${user.url}`,
-              ["icon_url"]: user.image,
-              name: user.username
-            },
-            description: `[@${user.username}](https://repl.it${user.url}) (${commanum(user.karma.toString())})
+      msg.channel.send({
+        embed: {
+          color: Colors.SUCCESS,
+          title: `User card for ${brackets(user.username)}`,
+          author: {
+            url: `https://repl.it${user.url}`,
+            ["icon_url"]: user.image,
+            name: user.username
+          },
+          description: `[@${user.username}](https://repl.it${user.url}) (${commanum(user.karma.toString())})
 **ID**: ${user.id}\n${codestr(user.bio, "txt")}`,
-            thumbnail: {
-              url: user.image
-            },
-            fields: user.languages.map(itm => ({
-              name: itm.displayName,
-              value: itm.tagline,
-              inline: true
-            }))
-          }
-        });
-      }
+          thumbnail: {
+            url: user.image
+          },
+          fields: user.languages.map(itm => ({
+            name: itm.displayName,
+            value: itm.tagline,
+            inline: true
+          }))
+        }
+      });
     }
   }
 }
