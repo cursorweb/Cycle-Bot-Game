72a93d723f89beb609272ce651f0c910d5199294
Junhao Zhang <junhaodev@gmail.com>
2022-01-14 16:28:29 -0600

    Updated version to 0.1.18

--- src/cmd/meta/version.ts
+++ src/cmd/meta/version.ts
@@ -10,8 +10,8 @@ class C extends Command {
     msg.channel.send({
       embeds: [{
         color: Colors.PRIMARY,
-        title: "Version 0.1.17",
-        description: "Prestige has been added! After like a month with no updates, we are back!!\nTo start, do `&social-media`.",
+        title: "Version 0.1.18",
+        description: "Quality of life update! We are now using discord.js v13, and many commands have been revamped!",
         fields: [{
           name: "Discord Server",
           value: "Talk to the developers, and get sneak peeks in the official [discord server](https://discord.gg/4vTPWdpjFz)!"
