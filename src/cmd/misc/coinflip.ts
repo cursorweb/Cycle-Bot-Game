2cd3c8aca2b57a2c2a69ce45ead9c00edf3186ed
Junhao Jerry Zhang <jerry_mountain@hotmail.com>
2022-08-28 17:36:20 -0500

    added coin flip

---
+++ src/cmd/misc/coinflip.ts
@@ -0,0 +1,24 @@
+import * as Discord from "discord.js";
+import { Command, Colors, hidden, randomChoice } from "../../global.js";
+
+class C extends Command {
+  names = ["coinflip", "coin-flip"];
+  help = "Flip a coin!";
+  examples = ["coinflip", "coin-flip"];
+  isGame = "n" as const;
+
+  // don't forget to change!
+  isAdmin = true;
+
+  exec(msg: Discord.Message, _: string[], _1: Discord.Client) {
+    msg.channel.send({
+      embeds: [{
+        color: Colors.SUCCESS,
+        title: "Coin flip!",
+        description: `You flipped a coin, and got ${hidden(randomChoice(["heads", "tails"])[0])}!`
+      }]
+    });
+  }
+}
+
+export const c = new C();
